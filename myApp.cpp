5e5efc1078da1a90675903d169e98b9b4622df3d
Ali Hamza <alihamza022bb@gmail.com>
2023-04-23 15:18:02 +0500

    committed

--- myApp.cpp
+++ myApp.cpp
@@ -2,19 +2,11 @@
 using namespace std;
 
 int main() {
-	int i = 1, a = 0, b = 1, sum = 0, num;
-	cout << "Enter a number:";
-	cin >> num;
-	cout << a << endl;
-	cout << b << endl;
-	while (i < num) {
-		sum = a + b;
-		cout << sum << endl;
-		i++;
-		a = b;
-		b = sum;
-	}
-	cout << "Hello...";
-	
+	int i = 10;
+	do {
+		cout << i << endl;
+		i--;
+	} while (i >= 1);
+
 	return 0;
 }
